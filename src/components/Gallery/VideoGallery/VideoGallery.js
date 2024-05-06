8e8b1748781be1953fd4d55ccc56998a227dec97
myunus-info <myunus_info@yahoo.com>
2024-05-06 13:27:38 +0600

    Department Part Done

--- src/components/Gallery/VideoGallery/VideoGallery.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const VideoGallery = () => {
-  return <div></div>;
-};
-
-export default VideoGallery;
--- src/components/pages/Departments/Departments.js
+++ src/components/pages/Departments/Departments.js
@@ -1,11 +1,106 @@
 import Layout from "../../Layout/Layout";
+import classes from "./Departments.module.css";
 
 export default function Departments() {
   return (
-    <>
-      <Layout>
-        <h1>This is Department page</h1>
-      </Layout>
-    </>
+    <Layout>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className={classes[`department-text`]}>
+              <h6>Departments</h6>
+              <h3 className={classes[`department-big-title`]}>
+                Our Departments
+              </h3>
+              <h5>Play</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                perferendis provident aliquid neque, commodi rerum a dolorum
+                cumque repellat quod, dolor et ut, sunt ipsam non labore quo ex
+                exercitationem nemo qui autem porro vitae. Veritatis
+                reprehenderit culpa, accusamus optio tempore possimus tempora
+                dolor distinctio et deleniti corporis doloremque vero!
+              </p>
+              <h5>Nursery</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                perferendis provident aliquid neque, commodi rerum a dolorum
+                cumque repellat quod, dolor et ut, sunt ipsam non labore quo ex
+                exercitationem nemo qui autem porro vitae. Veritatis
+                reprehenderit culpa, accusamus optio tempore possimus tempora
+                dolor distinctio et deleniti corporis doloremque vero!
+              </p>
+              <h5>Grade One</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                perferendis provident aliquid neque, commodi rerum a dolorum
+                cumque repellat quod, dolor et ut, sunt ipsam non labore quo ex
+                exercitationem nemo qui autem porro vitae. Veritatis
+                reprehenderit culpa, accusamus optio tempore possimus tempora
+                dolor distinctio et deleniti corporis doloremque vero!
+              </p>
+              <h5>Grade Two</h5>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+                sit in iure quisquam soluta dicta atque voluptatem, voluptates,
+                amet eaque corrupti quis est? Cum enim fugit laborum laudantium
+                tempore eum cupiditate facilis, cumque placeat amet at vel
+                tenetur quod accusamus blanditiis commodi ipsum similique
+                aperiam doloribus nostrum temporibus optio. Perspiciatis?
+              </p>
+              <h5>Grade Three</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id
+                amet, dicta quo sint nobis dolorum soluta ut, laborum nulla
+                tempora quis voluptatem velit saepe explicabo! Sit ab similique
+                consequatur nesciunt error cupiditate, earum, quidem
+                perspiciatis facere fugiat consequuntur laboriosam reiciendis
+                ratione ipsa molestiae saepe. Neque similique impedit officiis
+                voluptas dignissimos.
+              </p>
+              <h5>Grade Four</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit
+                magni sunt unde quia nemo esse provident, ea recusandae officiis
+                ex commodi obcaecati itaque aliquid perspiciatis molestias ad
+                suscipit dolorum! Velit at praesentium maiores necessitatibus a
+                delectus fugiat excepturi voluptatum, omnis quisquam ea eos
+                labore mollitia harum quaerat explicabo autem beatae?
+              </p>
+              <h5>Grade Five</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dignissimos iusto a, numquam eligendi eveniet, minus veritatis
+                libero commodi soluta ab cupiditate animi quam veniam, optio
+                exercitationem laudantium repellendus eos dicta consequatur?
+                Similique, doloremque dignissimos laborum voluptatum asperiores
+                optio itaque eaque deleniti mollitia adipisci sit doloribus
+                perferendis ad inventore voluptas animi?
+              </p>
+              <h5>Grade Six</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dignissimos iusto a, numquam eligendi eveniet, minus veritatis
+                libero commodi soluta ab cupiditate animi quam veniam, optio
+                exercitationem laudantium repellendus eos dicta consequatur?
+                Similique, doloremque dignissimos laborum voluptatum asperiores
+                optio itaque eaque deleniti mollitia adipisci sit doloribus
+                perferendis ad inventore voluptas animi?
+              </p>
+              <h5>Grade Seven</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dignissimos iusto a, numquam eligendi eveniet, minus veritatis
+                libero commodi soluta ab cupiditate animi quam veniam, optio
+                exercitationem laudantium repellendus eos dicta consequatur?
+                Similique, doloremque dignissimos laborum voluptatum asperiores
+                optio itaque eaque deleniti mollitia adipisci sit doloribus
+                perferendis ad inventore voluptas animi?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 }
