ef879d16a9055e4d4593c88a672def140a19455e
myunus-info <myunus_info@yahoo.com>
2024-05-06 09:37:57 +0600

    Admission Section Done

--- src/components/pages/Admission/Admission.js
+++ src/components/pages/Admission/Admission.js
@@ -4,8 +4,116 @@ import classes from "./Admission.module.css";
 export default function Admission() {
   return (
     <Layout>
-      <div className={classes["admission-text"]}>
-        <h1>This is Admission page</h1>;
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className={classes[`admission-text`]}>
+              <h6>ADMISSION</h6>
+              <h3 className={classes[`history-big-title`]}>
+                Why Study at GKMIS?
+              </h3>
+              <h5>Academic Scenario</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Temporibus esse, libero quasi culpa suscipit iusto alias, eos
+                illum excepturi deleniti dicta ea illo repellendus ullam
+                veritatis autem. Necessitatibus deleniti, vel non dignissimos
+                explicabo consequuntur ullam, vitae consectetur et a illum iste?
+                Accusamus, doloribus tempora, fugit cum quasi quo eos, quis ex
+                ipsum distinctio similique nisi fuga excepturi sequi!
+                Dignissimos voluptas voluptate at est doloribus quibusdam
+                perspiciatis sed sapiente amet culpa cumque ut numquam pariatur
+                quis voluptates possimus deserunt, alias eos mollitia ratione
+                omnis quisquam atque accusantium? Reprehenderit et obcaecati
+                consequuntur explicabo, similique ad illo facere voluptatibus
+                asperiores, odit cumque fugit quod possimus porro odio
+                perferendis voluptas nemo maxime? Dolor fugiat doloremque alias
+                beatae eaque quasi id libero. Laboriosam magnam harum earum
+                consequuntur, aut voluptatem voluptates magni quod nam, quasi
+                maiores fuga suscipit assumenda corrupti vitae ipsum est saepe
+                accusamus. Ipsam, beatae totam tenetur vero voluptatem aut
+                inventore voluptate fugiat magnam?
+              </p>
+              <h5>Classrooms</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+                odit dolor porro reiciendis aspernatur. Eum, enim? Sapiente
+                dolor dignissimos temporibus ipsa inventore voluptas, quo
+                ducimus minima earum, labore atque similique dolores adipisci
+                iure delectus ullam explicabo tenetur, recusandae consectetur
+                modi. Aspernatur quam quibusdam optio aliquam unde at sunt ab
+                eveniet.
+              </p>
+              <h5>Library</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+                ad placeat necessitatibus minus maiores! Non, itaque inventore.
+                Sed esse minus eveniet molestiae! Sint laborum molestiae culpa
+                pariatur cumque porro dicta quae animi numquam officiis nostrum
+                quibusdam vitae iure quos in voluptatem, perspiciatis voluptates
+                ut excepturi nam esse repellendus adipisci? Vel?
+              </p>
+              <h5>Co-Curricular Activities</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
+                explicabo qui numquam dolorum aspernatur commodi voluptates
+                labore praesentium officia. Animi voluptatibus provident quidem
+                dicta ex praesentium eaque fugit ipsa, saepe tenetur totam
+                dolores maxime adipisci, delectus perferendis at veritatis
+                quibusdam illo eius porro, vero ut ducimus. Incidunt debitis
+                accusantium soluta est nulla at modi commodi quidem perspiciatis
+                maiores ducimus praesentium architecto assumenda ipsam nisi
+                culpa aliquam cumque dolor, amet esse eum quasi nihil corporis
+                natus. Quaerat cum tenetur ipsum fuga quos recusandae eaque
+                explicabo a placeat odit voluptates commodi, numquam rem est
+                facilis, aliquid nisi eligendi, similique laudantium? Tempora,
+                perferendis.
+              </p>
+              <h5>Tuition Waiver</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Architecto qui veritatis dolores. Voluptatem maiores nobis
+                quidem ab velit possimus quaerat, dolor nam animi eligendi.
+                Dolore sunt corrupti, vero optio cumque facere veniam. Est
+                soluta natus incidunt, temporibus quasi, sunt molestiae
+                asperiores sequi deleniti consectetur dolore vero dignissimos,
+                voluptatibus repudiandae? Atque.
+              </p>
+              <h5>Achievements</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                amet a enim, molestiae tempore obcaecati rem nobis delectus odio
+                quasi quibusdam impedit beatae ea doloribus ipsa optio
+                asperiores id labore repellendus autem iure? Aliquam, optio.
+                Voluptate repudiandae nesciunt earum voluptatibus laudantium
+                corporis veniam hic sequi ipsa quae minus repellendus a labore
+                assumenda ullam minima quod ratione, ipsum iste officiis at
+                aspernatur temporibus. Eos incidunt magnam consectetur
+                reprehenderit quibusdam? Odit quia delectus fuga quos, maxime
+                suscipit impedit possimus incidunt tempore illo magnam nihil
+                veniam maiores, quae quis, nostrum officia. Inventore, sequi?
+                Sint rerum illo itaque officia recusandae placeat molestiae,
+                accusantium consequatur.
+              </p>
+              <h5>Success Story</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                amet a enim, molestiae tempore obcaecati rem nobis delectus odio
+                quasi quibusdam impedit beatae ea doloribus ipsa optio
+                asperiores id labore repellendus autem iure? Aliquam, optio.
+                Voluptate repudiandae nesciunt earum voluptatibus laudantium
+                corporis veniam hic sequi ipsa quae minus repellendus a labore
+                assumenda ullam minima quod ratione, ipsum iste officiis at
+                aspernatur temporibus. Eos incidunt magnam consectetur
+                reprehenderit quibusdam? Odit quia delectus fuga quos, maxime
+                suscipit impedit possimus incidunt tempore illo magnam nihil
+                veniam maiores, quae quis, nostrum officia. Inventore, sequi?
+                Sint rerum illo itaque officia recusandae placeat molestiae,
+                accusantium consequatur.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
