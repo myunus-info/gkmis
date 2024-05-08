798d27d4c875d01f2d33e743d430966dd7a2977b
myunus-info <myunus_info@yahoo.com>
2024-05-08 17:32:41 +0600

    Departments Done

--- src/components/pages/Departments/Bangla/Bangla.js
+++ src/components/pages/Departments/Bangla/Bangla.js
@@ -1,8 +1,161 @@
 import React from "react";
 import Layout from "../../../Layout/Layout";
+import check from "../../../icons/check-circle.png";
+import classes from "./Bangla.module.css";
 
 const Bangla = () => {
-  return <Layout>This is Bangla</Layout>;
+  return (
+    <Layout>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className={classes[`bangla-text`]}>
+              <h6>BANGLA</h6>
+              <h3 className={classes[`bangla-text-big-title`]}>
+                Department of Bangla
+              </h3>
+
+              <h5>Brief History</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem
+                eos aspernatur cumque, cum maiores officiis quos quam quas ipsa
+                nam sit rerum blanditiis ut nulla dolore sequi explicabo facilis
+                beatae mollitia iure! Dicta totam tenetur ad ipsum eos expedita
+                eveniet saepe nemo, officia doloremque. Dolore minus molestiae
+                dolores dolor consectetur.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                earum, voluptate quos optio aliquam totam repudiandae,
+                reprehenderit unde corporis minima quas molestias voluptates
+                excepturi sit accusamus non ex deleniti voluptatibus. Culpa
+                molestiae accusantium soluta! Iusto at repellendus, dicta
+                nostrum in culpa id praesentium minus nobis expedita quidem
+                optio. Ratione ab consequuntur saepe amet alias, quis voluptate
+                veritatis quod qui placeat est? Sint perspiciatis possimus sed
+                aperiam earum placeat ullam voluptatem similique nesciunt
+                officia repudiandae deleniti, mollitia ad maxime ab at fugiat
+                praesentium porro voluptatibus! Eum ducimus aliquam molestias
+                nostrum illo incidunt impedit provident obcaecati, sapiente
+                ratione, officia distinctio atque modi!
+              </p>
+              <h5>MiSSION</h5>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus ut numquam repudiandae autem beatae quia obcaecati.
+                Id a accusamus voluptatibus, cupiditate tempore repellendus est
+                maiores magni molestiae corporis molestias beatae repudiandae
+                nulla pariatur! Reprehenderit velit adipisci temporibus est
+                porro! Quam pariatur esse at inventore ipsa impedit, illum
+                temporibus voluptatum! Animi laboriosam sit fuga aperiam,
+                similique iusto impedit voluptatum. Eum nihil asperiores iure
+                ipsa similique quia tempora, consequatur architecto iusto quidem
+                possimus, labore hic. Possimus corporis labore voluptatum minus,
+                voluptates dignissimos. Cum eaque exercitationem, quam
+                perspiciatis sed neque incidunt praesentium quia quibusdam,
+                iure, quisquam in aliquid! Modi molestiae doloremque placeat
+                voluptate.
+              </p>
+              <h5>VISION</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Perspiciatis quas ducimus ad beatae ipsum cupiditate dolorem
+                quibusdam quod explicabo omnis ipsa ut necessitatibus minus
+                ullam deserunt, vitae libero delectus eum ratione architecto
+                iusto odit eaque maiores! Tempora deleniti maiores quos fugiat
+                illum qui praesentium eius ipsum quisquam a fuga possimus
+                voluptatem harum, minus minima quod officiis ut, placeat dolor
+                magnam facere sapiente alias! Illo quos possimus voluptatum
+                consectetur perspiciatis nulla ut asperiores, facilis
+                voluptatibus corporis minus consequatur minima similique
+                perferendis cupiditate mollitia dolore quas quis at voluptates
+                labore placeat rerum? Ipsum voluptas numquam inventore
+                distinctio quidem expedita dolore vitae laboriosam?
+              </p>
+
+              <h5>What expertise do we build up?</h5>
+              <ul>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+              </ul>
+
+              <h5>Objectives</h5>
+              <ul>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+                <li>
+                  <img src={check} alt="check" />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium, quam?
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Bangla;
