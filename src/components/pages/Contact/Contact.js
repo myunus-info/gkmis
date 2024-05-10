eda737fd69e8f05b308da1980ea5f5aa3cf3c6a6
myunus-info <myunus_info@yahoo.com>
2024-05-10 12:02:15 +0600

    Contact Page Done

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -74,11 +74,26 @@ const Contact = () => {
                 </div>
                 <div>
                   <p style={{ fontWeight: "700" }}>Visit Us</p>
-                  <p style={{ marginTop: "-1rem" }}>principalgkmis@gmail</p>
+                  <p style={{ marginTop: "-1rem" }}>
+                    Behind Gopaldi Girls' High School, Islampur, Gopaldi,
+                    Araihazar, Narayanganj
+                  </p>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+        <div>
+          {/* Google Map */}
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.4524910333266!2d90.70786917451922!3d23.80250348677575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37543700513cb5e3%3A0x92aa8c3e9788afc2!2sGopaldi%20K%20M%20International%20School!5e0!3m2!1sen!2sbd!4v1715317984234!5m2!1sen!2sbd"
+            width="100%"
+            height="450"
+            loading="lazy"
+            title="gkmis-location"
+            referrerpolicy="no-referrer-when-downgrade"
+            iframe
+          />
         </div>
       </div>
     </Layout>
